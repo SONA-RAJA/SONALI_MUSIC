d696cf390022437e7268d3da72bd30265a6f1524
SONA-RAJA <botspurvi@gmail.com>
2025-01-07 17:57:57 +0530

    Update play.py

--- SONALI/utils/inline/play.py
+++ SONALI/utils/inline/play.py
@@ -76,10 +76,10 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text=" 𝗝ᴏɪɴ 🤌 ", url="https://t.me/PURVI_SUPPORT",
+                text="˹ ❍ᴡηєʀ ˼", url="https://t.me/PURVI_SUPPORT",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text="˹ 𝛅ᴜᴘᴘσʀᴛ ˼", url="https://t.me/+1NPyVr47HYtiYWE9",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -99,10 +99,10 @@ def stream_markup(_, videoid, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝗝ᴏɪɴ 🤌", url="https://t.me/PURVI_SUPPORT",
+                text="˹ ❍ᴡηєʀ ˼", url="https://t.me/PURVI_SUPPORT",
             ),
             InlineKeyboardButton(
-                text="🌸 𝗦ᴜᴘᴘᴏʀᴛ 🌸", url="https://t.me/PURVI_UPDATES",
+                text="˹ 𝛅ᴜᴘᴘσʀᴛ ˼", url="https://t.me/+1NPyVr47HYtiYWE9",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
